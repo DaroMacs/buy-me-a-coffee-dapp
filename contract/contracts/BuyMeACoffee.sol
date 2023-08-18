d472bef74d71e2ba99f5d7f54dfc2d7e7ad329fc
DaroMacs <dariomacedog@gmail.com>
2023-08-17 20:55:10 -0600

    Hardhat compile and deploy added

---
+++ contract/contracts/BuyMeACoffee.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract BuyMeACoffee{
+
+    struct Memo {
+        string Name;
+        string message;
+         uint timeStamp;
+         address from;
+
+    }
+
+    Memo [] memos;
+    address payable owner;
+
+    constructor(){
+        owner= payable(msg.sender);
+    }
+
+    function buyChai(string calldata name, string calldata message) external payable {
+        require(msg.value > 0, 'Amount should be greater than 0');
+        owner.transfer(msg.value);
+        memos.push(Memo(name, message, block.timestamp, msg.sender)); 
+    }
+
+    function getMemos() public view returns(Memo[] memory){
+        return memos;
+    }
+}
--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
+
+//0x200da7D8D222Fbd5dc98f154B8b714FB5D38CA9C
